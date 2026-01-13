7ecb43d502889e22e3567fd6e6055533d7ac2d3f
MarinaRocha2911 <marina.monteiro@colaborador.embrapa.br>
2026-01-13 16:18:12 -0300

    Update Features.py

--- Features.py
+++ Features.py
@@ -1,6 +1,6 @@
 lista_v0 = ['ano','indice_area_citros','area_citros']
 
-# lista definida originalmente - possui todas as variaveis possiveis
+#list_v1 original list - contains all of the available features
 lista_v1 = ['ano','area_citros','indice_area_citros','percentual_v123','T_15',
        'Tmin_15', 'Tmax_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100', 'indice_area_citros', 'area_citros', 
        'T_15_d1', 'Trange_15_d1', 'Tabsmin_15_d1', 'Tabsmax_15_d1',
@@ -16,10 +16,9 @@ lista_v1 = ['ano','area_citros','indice_area_citros','percentual_v123','T_15',
        'prec_15_d1', 'prec_15_d2', 'prec_15_d3', 'prec_15_d4', 'prec_15_d5',
        'prec_15_d6', 'prec_15_d7', 'prec_15_d8', 'balanco_h_cad100_d1', 'balanco_h_cad100_d2',
        'balanco_h_cad100_d3', 'balanco_h_cad100_d4', 'balanco_h_cad100_d5',
-       'balanco_h_cad100_d6', 'balanco_h_cad100_d7', 'balanco_h_cad100_d8'] #'gravidade_ano_anterior'?
+       'balanco_h_cad100_d6', 'balanco_h_cad100_d7', 'balanco_h_cad100_d8']
 
-
-#v2 apenas com absolutos minimo e maximo (em temperaturas)
+#list_v2 contains only absolute minimum ans maximum temperature values
 lista_v2 = ['ano','gravidade_ano_anterior_4_meso','percentual_v123','total_armadilhas',
             'prop_manejos_4_meso','razao_total','area_citros','indice_area_citros',
             'T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100', 
@@ -38,19 +37,19 @@ lista_v2 = ['ano','gravidade_ano_anterior_4_meso','percentual_v123','total_armad
        'balanco_h_cad100_d3', 'balanco_h_cad100_d4', 'balanco_h_cad100_d5',
        'balanco_h_cad100_d6', 'balanco_h_cad100_d7', 'balanco_h_cad100_d8']
 
-#v3 = v2 mas sem delays
+#list_v3 is equal to list_v2 but without the delayed features
 lista_v3 = ['ano','gravidade_ano_anterior_4_meso','percentual_v123','total_armadilhas',
             'prop_manejos_4_meso','razao_total','area_citros','indice_area_citros',
             'T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100']
 
-#v4 lista super simplificada - v3 sem grav anterior, percentual v123 e total armadilhas
+#list_v4 is super simplified, equals list_v3 but without 'grav_anterior', 'percentual_v123' and 'total_armadilhas'
 lista_v4 = ['ano','indice_area_citros','area_citros','razao_total','prop_manejos_4_meso',
             'T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100']
 
-#v5 apenas clima atual, sem dados de area de citros, tipo de manejo, etc
+#list_v5 contains only current climate features, without citrus related features
 lista_v5 = ['ano','T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100']
 
-#apenas clima, mas atual e com delays
+#list_v6 contains only climate features, including current and delayed
 lista_v6 = ['ano','T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100', 
        'T_15_d1', 'Trange_15_d1', 'Tabsmin_15_d1', 'Tabsmax_15_d1',
        'T_15_d2', 'Trange_15_d2', 'Tabsmin_15_d2',
@@ -67,7 +66,7 @@ lista_v6 = ['ano','T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'b
        'balanco_h_cad100_d3', 'balanco_h_cad100_d4', 'balanco_h_cad100_d5',
        'balanco_h_cad100_d6', 'balanco_h_cad100_d7', 'balanco_h_cad100_d8']
 
-# v2 sem 'gravidade_ano_anterior_4_meso','percentual_v123','total_armadilhas','prop_manejos_4_meso'
+#list_v7 is equal to list_v2 but without 'gravidade_ano_anterior_4_meso','percentual_v123','total_armadilhas' and 'prop_manejos_4_meso'
 lista_v7 = ['ano','razao_total','area_citros','indice_area_citros',
             'T_15', 'Trange_15', 'Tabsmin_15', 'Tabsmax_15', 'prec_15', 'balanco_h_cad100', 
        'T_15_d1', 'Trange_15_d1', 'Tabsmin_15_d1', 'Tabsmax_15_d1',
@@ -85,8 +84,7 @@ lista_v7 = ['ano','razao_total','area_citros','indice_area_citros',
        'balanco_h_cad100_d3', 'balanco_h_cad100_d4', 'balanco_h_cad100_d5',
        'balanco_h_cad100_d6', 'balanco_h_cad100_d7', 'balanco_h_cad100_d8']
 
-# 30 melhores features de acordo com o modelo padrao treinado com lista_v7
-# percebe-se que os modelos aprendem melhor com menos variaveis preditoras, por isso selecionamos as que mais fizeram diferenca e retreinamos
+#list_v8 contains the top 30 features according to the feature importance estimated by training the model with list_v7
 lista_v8 = ['ano','T_15_d4', 'balanco_h_cad100_d3', 'Tabsmax_15_d7', 'Tabsmax_15_d6', 'Tabsmin_15_d1', 'prec_15_d3', 
             'Trange_15_d1', 'Tabsmax_15_d1', 'T_15_d5', 'Tabsmax_15', 'Tabsmin_15_d4', 'Trange_15', 'Tabsmin_15_d3', 
             'Trange_15_d8', 'Trange_15_d2', 'Tabsmax_15_d8', 'Tabsmin_15_d7', 'T_15_d7', 'Tabsmin_15_d5', 'T_15_d6', 
